d6400979b6209ca0f20ef779212440e47b6d226c
ritchie-gr8 <rit.yotsathon@gmail.com>
2024-04-11 15:11:33 +0700

    feat: add answers

--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,35 @@
-class Notification { }
+class Notification {
+    constructor(notificationId, createdTime, content, receiver) {
+        this.notificationId = notificationId
+        this.createdTime = createdTime
+        this.content = content
+        this.receiver = receiver
+    }
 
-class EmailNotification { }
+    send() {
+        console.log(`Notification has been sent to ${this.receiver}`)
+    }
+}
 
-class SMSNotification { }
+class EmailNotification extends Notification {
+    constructor(notificationId, createdTime, content, receiver) {
+        super(notificationId, createdTime, content, receiver)
+    }
+}
+
+class SMSNotification extends Notification {
+    constructor(notificationId, createdTime, content, phoneNumber) {
+        super(notificationId, createdTime, content, phoneNumber)
+    }
+}
+
+
+const sendEmail = new EmailNotification(
+    1, new Date(), "this is email content", "test@email.com"
+)
+sendEmail.send()
+
+const sendSms = new SMSNotification(
+    1, new Date(), "this is sms content", "0889997777"
+)
+sendSms.send()
